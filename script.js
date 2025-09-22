97f24ad53196b99c50357ad540384f9752c5e01d
Janhavi gupta <janhaviii0408@gmail.com>
2025-09-22 14:39:29 +0530

    cahnges

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const animeSection = document.getElementById("animeSection");
 const searchInput = document.getElementById("searchInput");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
+const filter = document.getElementById("filter");
+const sort = document.getElementById("sort");
 
 let currentPage = 1;
 let currentSearch = "";
@@ -12,53 +14,57 @@ async function loadAnime(searchTerm = "", page = 1) {
 
   try {
     const response = await fetch(`${API_URL}?page=${page}&limit=6&q=${searchTerm}`);
-    const data = await response.json();
+    const result = await response.json();
 
     animeSection.innerHTML = "";
 
-    if (!data.data.length) {
+    if (result.data.length === 0) {
       animeSection.innerHTML = "<p>No anime found.</p>";
       return;
     }
 
-    data.data.forEach(anime => {
+    result.data.forEach( function(anime){
       const card = document.createElement("div");
       card.classList.add("amine-card");
 
       card.innerHTML = `
-        <img src="${anime.images.jpg.image_url}" alt="${anime.title}">
+        <img src="${anime.images.jpg.image_url}">
         <h2>${anime.title}</h2>
         <p>${anime.year || "Unknown Year"}</p>
       `;
 
       animeSection.appendChild(card);
     });
+    
 
     prevBtn.disabled = page === 1;
-    nextBtn.disabled = !data.pagination.has_next_page;
+    nextBtn.disabled = !result.pagination.has_next_page;
 
   } 
   catch (error) {
-    animeSection.innerHTML = "<p>Failed to load anime. Please try again.</p>";
+    animeSection.innerHTML = "<p>Failed to load. Please try again.</p>";
   }
 }
 
-searchInput.addEventListener("input", (e) => {
+searchInput.addEventListener("input", function(e) {
   currentSearch = e.target.value;
   currentPage = 1;
   loadAnime(currentSearch, currentPage);
 });
 
-prevBtn.addEventListener("click", () => {
+
+prevBtn.addEventListener("click", function() {
   if (currentPage > 1) {
     currentPage--;
     loadAnime(currentSearch, currentPage);
   }
 });
 
-nextBtn.addEventListener("click", () => {
+
+nextBtn.addEventListener("click", function() {
   currentPage++;
   loadAnime(currentSearch, currentPage);
 });
+
 
 loadAnime();
